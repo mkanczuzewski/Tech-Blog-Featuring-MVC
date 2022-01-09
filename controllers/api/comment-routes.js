cd49efd52885dd5490788e702fa9158108977ee4
mkanczuzewski <mkanczuzewski@gmail.com>
2022-01-09 09:47:48 -0500

    comment routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -9,3 +9,21 @@ router.get('/', (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router.post('/', (req, res) => {
+ // if (req.session) {
+    Comment.create({
+        comment_text: req.body.comment_text,
+      //user_id: req.session.user_id,
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+    })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+    });
+//  }
+});
+
+module.exports = router;
