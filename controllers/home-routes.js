917f663112bdc4a28e25b61cd244fbbddd3887d0
mkanczuzewski <mkanczuzewski@gmail.com>
2022-01-09 15:05:01 -0500

    add in homepage and login page

---
+++ controllers/home-routes.js
@@ -0,0 +1,42 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+  Post.findAll({
+    attributes: [
+      'id',
+      'title',
+      'created_at',
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+        // pass a single post object into the homepage template
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('homepage', { posts });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-//const path = require('path');
+const bootstrap = require("express-bootstrap-service");
 const express = require('express');
 //const session = require('express-session');
 const controllers = require('./controllers');
-//const exphbs = require('express-handlebars');
-
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 const sequelize = require('./config/connection');
 // const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -24,14 +23,12 @@ const sequelize = require('./config/connection');
 
 //const helpers = require('./utils/helpers');
 
-//const hbs = exphbs.create({ helpers });
-
-//app.engine('handlebars', hbs.engine);
-//app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(bootstrap.serve);
 
 app.use(require('./controllers/'));
 
