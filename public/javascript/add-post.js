c6017db4685ba221fdb3e410ece09c96f08cf17c
mkanczuzewski <mkanczuzewski@gmail.com>
2022-01-11 15:03:54 -0500

    Dashboard add post functionality

---
+++ public/javascript/add-post.js
@@ -0,0 +1,26 @@
+async function newFormHandler(event) {
+    event.preventDefault();
+  
+    const title = document.querySelector('input[name="post-title"]').value;
+    const post_text = document.querySelector('textarea[name="post-text"]').value;
+    console.log(title);
+    console.log(post_text);
+    const response = await fetch(`/api/posts`, {
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+        post_text
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
